e0e2ac541dd50d6a4dcd3dff5b75f880596140eb
proyectoiotmflr <82426683+proyectoiotmflr@users.noreply.github.com>
2021-04-12 22:26:43 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,45 +1,10 @@
-import {
-  getFirestore
-} from "../lib/fabrica.js";
-import {
-  cod, muestraError
-} from "../lib/util.js";
-
-const tbody =
-  document.querySelector("tbody");
-
-renderHistorial();
-async function renderHistorial() {
-  try {
-    let html = "";
-    const snap =
-      await getFirestore().
-        collection("Historial").
-        orderBy("timestamp").
-        get();
-    snap.forEach(doc => {
-      const data = doc.data();
-      html += /* html */
-        `<tr>
-          <td>
-            ${cod(doc.id)}
-          </td>
-          <td>
-            ${cod(
-          data.dispositivoId)}
-          </td>
-          <td>
-            ${cod(data.valor.
-            toString())}
-          </td>
-          <td>
-            ${cod(data.timestamp.
-              toString())}
-          </td>
-        </tr>`;
-    });
-    tbody.innerHTML = html;
-  } catch (e) {
-    muestraError(e);
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `&copy; 2021
+      María Fernanda López Rodríguez`;
   }
 }
+customElements.define(
+  "mi-footer", MiFooter);
